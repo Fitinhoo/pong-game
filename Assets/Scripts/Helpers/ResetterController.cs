8899d042d1548b9d41c7b9ea212df791de248a8c
Fitinho <claudiocdomene@gmail.com>
2021-03-08 01:53:47 -0300

    Add Reset system

---
+++ Assets/Scripts/Helpers/ResetterController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ResetterController : SingletonMonobehaviour<ResetterController>
+{
+    public void ResetAll()
+    {
+        var resettables = InterfaceHelper.FindObjects<IResettable>();
+        foreach (var resettable in resettables)
+            resettable.OnReset();
+    }
+}
+
