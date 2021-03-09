b8f4b841afd64b51f1f321ad1b11d1cb8aecb141
Fitinho <claudiocdomene@gmail.com>
2021-03-08 22:25:26 -0300

    Update levels controller

--- Assets/Scripts/GameManager/LevelsController.cs
+++ Assets/Scripts/GameManager/LevelsController.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class LevelsController : MonoBehaviour
 {
+    #region <--- VARIABLES --->
+    [Header("Settings: ")]
+    [SerializeField] private List<GameObject> levelsPrefab;
+    [Header("For Debug:")]
+    public int CurrentLevelIndex = default;
+    public int CurrentLevel = default;
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* ENGINE METHODS   ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
+    private void Start()
+    {
+        CurrentLevel = 0;
+        CurrentLevelIndex = 0;
+    }
+
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* PUBLIC METHODS   ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
     public void StartLevel()
     {
+        Instantiate(levelsPrefab[CurrentLevelIndex]);
         ResetterController.Instance.ResetAll();
     }
+
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* PRIVATE METHODS  ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
+
+
+    #endregion
 }
