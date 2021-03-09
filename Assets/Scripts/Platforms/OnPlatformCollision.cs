7fa2c3bba105d57ec3da18f367b1718bb813496a
Fitinho <claudiocdomene@gmail.com>
2021-03-09 02:38:08 -0300

    Add impact effect

--- Assets/Scripts/Platforms/OnPlatformCollision.cs
+++ Assets/Scripts/Platforms/OnPlatformCollision.cs
@@ -6,12 +6,20 @@ public class OnPlatformCollision : MonoBehaviour
 {
     #region <--- VARIABLES --->
     [SerializeField] private LayerMask layerToCollide;
+    [SerializeField] private GameObject impactEffectPrefab = default;
 
     #endregion
     #region <~~*~~*~~*~~*~~*~~* ENGINE METHODS   ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
     private void OnCollisionEnter(Collision other)
     {
-        if (layerToCollide == (1 << other.gameObject.layer)) OnCollision(other);
+        if (layerToCollide == (1 << other.gameObject.layer))
+        {
+            if (impactEffectPrefab != null)
+                InstantiateImpactEffect(other);
+
+            OnCollision(other);
+        }
+            
     }
 
 
@@ -24,6 +32,15 @@ public class OnPlatformCollision : MonoBehaviour
     {
         Vector3 normalCollision = new Vector3(other.contacts[0].normal.x, other.contacts[0].normal.y, 0);
         BallBehavior.Instance.ReverseMotion(normalCollision);
+    }
+
+
+    private void InstantiateImpactEffect(Collision other)
+    {
+        GameObject obj = Instantiate(impactEffectPrefab, other.contacts[0].point, Quaternion.identity);
+        obj.transform.up = new Vector2(other.contacts[0].normal.x, other.contacts[0].normal.y);
+        obj.GetComponent<ParticleSystem>().Play();
+        Destroy(obj, 2f);
     }
 
 
