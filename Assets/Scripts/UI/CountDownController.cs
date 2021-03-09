2f8549cfb208c96822edfa096f413ed8940417e5
Fitinho <claudiocdomene@gmail.com>
2021-03-08 22:24:27 -0300

    Add ui controllers

---
+++ Assets/Scripts/UI/CountDownController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CountDownController : SingletonMonobehaviour<CountDownController>
+{
+    #region <--- VARIABLES --->
+    [Header("References: ")]
+    [SerializeField] private TextMeshProUGUI actorUI = default;
+    [Header("Settings: ")]
+    [SerializeField] private int startCountdownNumber = default;
+    [SerializeField] private float smoothAnimationFactor = default;
+    [Header("Events: ")]
+    [SerializeField] private UnityEvent countdownFinished = default;
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* ENGINE METHODS   ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
+    private void Start()
+    {
+        actorUI.gameObject.SetActive(false);
+    }
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* PUBLIC METHODS   ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
+    public void StartCountdown() => StartCoroutine(CountdownCoroutine());
+
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* PRIVATE METHODS  ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
+    private IEnumerator CountdownCoroutine()
+    {
+        actorUI.gameObject.SetActive(true);
+        for(int i = startCountdownNumber; i > 0; i--)
+        {
+            actorUI.text = i.ToString();
+            for(float j = 1; j > 0; j -= smoothAnimationFactor)
+            {
+                actorUI.rectTransform.localScale = Vector3.one * j;
+                yield return new WaitForSeconds(smoothAnimationFactor);
+            }
+        }
+
+        countdownFinished?.Invoke();
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/UI/ScoreController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreController : SingletonMonobehaviour<ScoreController>, IResettable
+{
+    #region <--- VARIABLES --->
+    [Header("References: ")]
+    [SerializeField] private TextMeshProUGUI leftScore = default;
+    [SerializeField] private TextMeshProUGUI rightScore = default;
+    public int LeftScoreCount { get; private set; } = default;
+    public int RightScoreCount { get; private set; } = default;
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* ENGINE METHODS   ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
+    private void Start()
+    {
+        OnReset();
+    }
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* PUBLIC METHODS   ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
+    public void AddLeftScore()
+    {
+        LeftScoreCount++;
+        leftScore.text += "| ";
+    }
+
+
+    public void AddRightScore()
+    {
+        RightScoreCount++;
+        rightScore.text += "| ";
+    }
+
+    public void OnReset()
+    {
+        leftScore.text = rightScore.text = "";
+        RightScoreCount = LeftScoreCount = 0;
+    }
+
+    #endregion
+    #region <~~*~~*~~*~~*~~*~~* PRIVATE METHODS  ~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*>
+
+
+    #endregion
+}
